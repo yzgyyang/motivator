4f9ecce7e2a65ec2f8d6e2c36b424e59898f7c1f
Fondson Tran <fondsont@gmail.com>
2016-10-08 12:39:57 -0400

    partially working google sign-in

--- routes/firebase.js
+++ routes/firebase.js
@@ -1,10 +1,12 @@
 var firebase = require('firebase');
-var config = {
+var config = {	
     apiKey: "AIzaSyDk1aO_yaVKgTi9dGwY6-rkLVX2nka-QdU",
     authDomain: "motivator-a8e20.firebaseapp.com",
     databaseURL: "https://motivator-a8e20.firebaseio.com",
     storageBucket: "",
     messagingSenderId: "24548565731"
   };
+
+
 /*firebase auth*/
 firebase.initializeApp(config);
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,62 @@ var router = express.Router();
 /* GET home page. */
 // FirebaseUI config.
 var firebase = require('./firebase.js');
+
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express'});
 });
+
+function onSignIn(googleUser) {
+  console.log('Google Auth Response', googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.getAuthResponse().id_token);
+      // Sign in with credential from the Google user.
+      firebase.auth().signInWithCredential(credential).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+    
+
+if (firebase.currentUser != null) {
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: "+profile.providerId);
+    console.log("  Provider-specific UID: "+profile.uid);
+    console.log("  Name: "+profile.displayName);
+    console.log("  Email: "+profile.email);
+    console.log("  Photo URL: "+profile.photoURL);
+  });
+}
+  });
+}
+
+function isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 
 module.exports = router;
